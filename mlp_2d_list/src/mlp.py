fffc013b90b770408e8a7bab38afec259d60903e
Vegard Bergsvik Øvstegård <vegovs@gmail.com>
2018-10-30 02:02:39 +0100

    :construction: wip(mlp)
    mlp.backward Not working properly. Control deltas computations and fix weight updates.

--- mlp_2d_list/src/mlp.py
+++ mlp_2d_list/src/mlp.py
@@ -1,4 +1,5 @@
 import random
+from math import exp
 from pandas_ml import ConfusionMatrix
 
 
@@ -14,9 +15,9 @@ class Mlp:
         # Delta update for adding momentum
         self.update_v = [[0] * len(self.v[0])] * nhidden
         # Output layer weights with bias
-        self.w = [[random.uniform(-1.0, 1.0) for _ in range(nhidden + 1)] for _ in range(8)]
+        self.w = [[random.uniform(-1.0, 1.0) for _ in range(nhidden + 1)] for _ in range(len(targets[0]))]
         # Delta update for adding momentum
-        self.update_w = [[0] * (nhidden + 1)] * len(targets[0])
+        self.update_w = [[0] * nhidden] * len(targets[0])
         # Hidden layer activation
         self.a = [0] * nhidden
         # Output activation
@@ -75,32 +76,33 @@ class Mlp:
         :return: None
         """
 
+        # TODO: Control computation
         # Compute the error at the output
         delta_o = [0] * len(self.y)
-        for i in range(delta_o):
-            delta_o[i] = target[i] - self.y[i]
+        for i in range(len(delta_o)):
+            delta_o[i] = (target[i] - self.y[i]) * self.y[i] * (1 - self.y[i])
 
+        # TODO: Control computation
         delta_h = [0] * len(self.a)
         # Compute the weight * output error sum for the error in the hidden layer
-        sum_wd = [0] * len(delta_h)
-        for i in range(delta_h):
-            # Indexing weight +1 to avoid bias
-            sum_wd[i] = sum(self.w[k][i + 1] * delta_o[k] for k in range(delta_o))
         # Compute the error in the hidden layer
-        for i in range(delta_h):
-            delta_h[i] = self.a[i] * (1 - self.a[i]) * sum_wd[i]
+        for i in range(len(delta_h)):
+            # Indexing weight +1 to avoid bias
+            delta_h[i] = self.a[i] * (1 - self.a[i]) * sum(self.w[k][i + 1] * delta_o[k] for k in range(len(delta_o)))
 
+        # TODO: Fix weight updates
         # Update the output layer weights
-        for j in range(self.update_w):
-            for k in range(self.update_w[0]):
-                self.update_w[j][k] = -self.eta * delta_o[k] * self.a[j] + self.momentum * self.update_w[j][k]
-                self.w[j][k] += self.update_w
+        for k in range(len(self.update_w)):
+            for z in range(len(self.update_w[0])):
+                self.update_w[k][z] = -self.eta * delta_o[k] * self.a[z] + self.momentum * self.update_w[k][z]
+                self.w[k][z] += self.update_w[k][z]
 
         # Update the hidden layer weights
-        for j in range(self.update_v):
-            for k in range(self.update_v[0]):
-                self.update_v[j][k] = -self.eta * delta_h[k] * self.a[j] + self.momentum * self.update_v[j][k]
-                self.v[j][k] += self.update_v
+        bias_input = [-1.0] + input.tolist()
+        for k in range(len(self.update_v)):
+            for i in range(len(self.update_v[0])):
+                self.update_v[k][i] = -self.eta * delta_h[k] * bias_input[i] + self.momentum * self.update_v[k][i]
+                self.v[k][i] += self.update_v[k][i]
 
     def forward(self, inputs):
         """Computes the activation function from the hidden layer, to the output layer.
@@ -108,29 +110,31 @@ class Mlp:
         :param {array-like}, shape = [n_features] inputs: Training vector with 'n_features' features/inputs.
         :return: None
         """
+        # TODO: Control computation
         # Compute the activation of each neuron j in the hidden layer
-        for zeta in range(len(self.a)):
-            # Compute sigma(weighted sum with bias)
-            self.a[zeta] = self.sigmoid(np.dot(inputs, self.v[zeta][1:]) + self.v[zeta][0] * -1)
+        for z in range(len(self.a)):
+            # Compute weighted sum with bias
+            self.a[z] = self.sigmoid(sum(inputs[i] * self.v[z][i] for i in range(len(inputs))))
+        # TODO: Control computation
         # Compute the activation of each neuron j in the output layer
-        for kappa in range(len(self.y)):
+        for k in range(len(self.y)):
             # Compute sigma(weighted sum with bias)
-            self.y[kappa] = self.sigmoid(np.dot(self.a.transpose(), self.w[kappa][1:]) + self.w[kappa][0] * -1)
+            self.y[k] = self.sigmoid(sum(self.a[j] * self.w[k][j] for j in range(len(self.a))))
 
     def confusion(self, inputs, targets):
         """Prints the confusion matrix
-               :param inputs:
-               :param targets:
-               :return:
-               """
+       :param inputs:
+       :param targets:
+       :return:
+       """
         predicted = []
         expected = []
 
         # Produce confusion matrix arrays
-        for i, t in zip(inputs, targets):
+        for i, t in zip(inputs, targets.tolist()):
             self.forward(i)
-            predicted.append(np.argmax(self.y))
-            expected.append(np.argmax(t))
+            predicted.append(self.y.index(max(self.y)))
+            expected.append(t.index(max(t)))
 
         confusion_m = ConfusionMatrix(expected, predicted)
         confusion_m.print_stats()
@@ -141,7 +145,7 @@ class Mlp:
         :param float h: Weighted sum input
         :return : g(h)
         """
-        return 1.0 / (1.0 + np.exp(-self.beta * h))
+        return 1.0 / (1.0 + exp(-self.beta * h))
 
     def error(self, targets):
         """Computes the squared error sum
